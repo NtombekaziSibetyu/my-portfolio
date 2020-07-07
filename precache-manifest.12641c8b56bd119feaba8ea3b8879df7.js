450832e2cdc846ee10b22c62e295aad57a10a418
NtombekaziSibetyu <sntombekazi@gmail.com>
2020-07-07 13:13:51 +0200

    Updates

---
+++ precache-manifest.12641c8b56bd119feaba8ea3b8879df7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddf9fc94fbe45f3080c7ac3fdb07e663",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "013d623114aef2c26918",
+    "url": "/my-portfolio/static/css/main.2fb53825.chunk.css"
+  },
+  {
+    "revision": "3de59772bb672ebee47a",
+    "url": "/my-portfolio/static/js/2.6ea6de77.chunk.js"
+  },
+  {
+    "revision": "ddb934a41eb6bf13706c65fe3758fb10",
+    "url": "/my-portfolio/static/js/2.6ea6de77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "013d623114aef2c26918",
+    "url": "/my-portfolio/static/js/main.cb7e5a6b.chunk.js"
+  },
+  {
+    "revision": "f241adf7bba56ceeeb82",
+    "url": "/my-portfolio/static/js/runtime-main.8b00eace.js"
+  },
+  {
+    "revision": "1cbf80849ccd7b25e9e96bcb2dcd6132",
+    "url": "/my-portfolio/static/media/contact-keeper.1cbf8084.png"
+  },
+  {
+    "revision": "8aef9201b4923c2f8a9fd808032038d6",
+    "url": "/my-portfolio/static/media/github.8aef9201.png"
+  },
+  {
+    "revision": "ecf7c6d4d3feacbc9275cb0d98ec6df8",
+    "url": "/my-portfolio/static/media/it-logger.ecf7c6d4.png"
+  },
+  {
+    "revision": "3e12f72e70c9dbf50ca7f61ab58b85a9",
+    "url": "/my-portfolio/static/media/website.3e12f72e.png"
+  }
+]);
