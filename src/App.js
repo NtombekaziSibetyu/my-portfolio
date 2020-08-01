35c191b2dffe7db687a1196006160429ed5f5ad6
NtombekaziSibetyu <sntombekazi@gmail.com>
2020-08-01 23:17:55 +0200

    update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Fragment } from 'react';
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './components/layout/navbar/Navbar';
 import Home from './components/layout/landingPage/Home';
-import About from './components/pages/about/About';
+import About from './components/pages/about/About'
 import Contact from './components/pages/contact/Contact';
 import Portfolio from './components/pages/portfolio/Portfolio';
 
@@ -15,9 +15,9 @@ const App = () => {
           <Navbar/>
           <Switch>
               <Route path='/' component={Home}/>
-              <Route exact path='/about' component={About}/>
-              <Route exact path='/contact' component={Contact}/>
-              <Route exact path='/skills' component={Portfolio}/>
+              <Route path='/about' component={About}/>
+              <Route path='/contact' component={Contact}/>
+              <Route path='/skills' component={Portfolio}/>
             </Switch>
         </Fragment>
       </Router>
