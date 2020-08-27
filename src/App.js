0498ab050688f4fafcb4542e4f5583f7e6fab4fa
NtombekaziSibetyu <sntombekazi@gmail.com>
2020-08-27 12:12:50 +0200

    update

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
         <Fragment>
           <Navbar/>
           <Switch>
-              <Route exact path='/' component={Home}/>
+              <Route path='/' component={Home}/>
               <Route path='/about' component={About}/>
               <Route path='/contact' component={Contact}/>
               <Route  path='/skills' component={Portfolio}/>
