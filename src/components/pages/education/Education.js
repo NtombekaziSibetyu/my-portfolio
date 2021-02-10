990894f7976d0a20fad14123d85b48e3371f4e56
NtombekaziSibetyu <sntombekazi@gmail.com>
2021-02-10 09:34:14 +0200

    changes

--- src/components/pages/education/Education.js
+++ src/components/pages/education/Education.js
@@ -11,14 +11,14 @@ const Education = () => {
                     EDUCATION
                 </h3>
                 <h4><strong>Other  </strong></h4>
+                <h5>Year  :  2020</h5>
                 <h5>Life Choices Coding Academy</h5>
                 <h5>Course  :  Fullstack Web Development</h5>
-                <h5>Year  :  2020</h5>
-                
+               
                 <h4><strong>Tertiary  </strong></h4>
+                <h5>Year  :  2018</h5>
                 <h5>Institution : University of Western Cape</h5>
                 <h5>Qualification : BSc Biotechnology</h5>
-                <h5>Year  :  2018</h5>
                 <p>Open Certificate PDF file 
                     <a href={certificate} target='_blank'
                             rel="noopener noreferrer">
