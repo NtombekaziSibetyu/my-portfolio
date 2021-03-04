18d0b07e88b5157c1b6be9d87d67f3b77bc0129d
NtombekaziSibetyu <sntombekazi@gmail.com>
2021-03-04 08:54:05 +0200

    commit

--- src/components/layout/footer/Footer.js
+++ src/components/layout/footer/Footer.js
@@ -10,13 +10,13 @@ const Footer = () => {
             title="Github"><i class="fab fa-github"></i></a></li>
             <li><a class="icoLinkedin" href="https://www.linkedin.com/in/ntombekazi-sibetyu-21a353164 " target='_blank' rel="noopener noreferrer"
             title="Linkedin"><i class="fab fa-linkedin"></i></a></li>
-            <li><a class="icoTwitter" href="https://twitter.com/" target='_blank' rel="noopener noreferrer"
-            title="Twitter"><i class="fab fa-twitter"></i></a></li>
+            {/* <li><a class="icoTwitter" href="https://twitter.com/" target='_blank' rel="noopener noreferrer"
+            title="Twitter"><i class="fab fa-twitter"></i></a></li> */}
             
             
-            <li><a class="icoFacebook" href="https://www.facebook.com/NtombekaziSIbetyu" target='_blank' rel="noopener noreferrer"
-            title="Facebook"><i class="fab fa-facebook"></i></a></li>
-            <li><a class="icoInstagram" href="https://www.instagram.com/nsibetyu"
+            {/* <li><a class="icoFacebook" href="https://www.facebook.com/NtombekaziSibetyu" target='_blank' rel="noopener noreferrer"
+            title="Facebook"><i class="fab fa-facebook"></i></a></li> */}
+            <li><a class="icoInstagram" href="https://www.instagram.com/sibetyun"
              target='_blank' rel="noopener noreferrer" title="Instagram"><i class="fab fa-instagram"></i></a></li>
             </ul>
             </div>
